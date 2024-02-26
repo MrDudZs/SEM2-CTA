0f15c5efdb1639118d01e250b7b25890d44d1326
Matt <123038044+MrDudZs@users.noreply.github.com>
2024-02-26 21:36:44 +0000

    Create login.php

---
+++ login.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <script></script>
+    <link rel="stylesheet" type="text/css" href="css/mobile.css" />
+    <link rel="stylesheet" type="text/css" media="only screen and (min-width:601px)" href="css/desktop.css" />
+</head>
+
+<body>
+    <div class="container mob-container">
+        <div class="desk-header mob-header">
+            <header>
+                <div>
+                    <img class="desk-logo mob-logo" src="images/CompanyLogoBLK.jpg" alt="Company Logo">
+                </div>
+            </header>
+        </div>
+        <div class="nav-container">
+            <nav class="top-nav">
+                <ul>
+                    <li><a href="index.html">HOME</a></li>
+                    <li><a href="exchange.html">EXCHANGE</a></li>
+                    <li><a href="">RATES</a></li>
+                    <li><a href="">ABOUT</a></li>
+                    <li style="float:right"><a href="login.php">login</a></li>
+                </ul>
+            </nav>
+        </div>
+        <div class="login-main">
+            <main>
+                <div class="login-container-mn">
+                    <h2>Login</h2>
+                </div>
+            </main>
+        </div>
+    </div>
+</body>
+
+</html>
