56af352bed7bba69369b2e449ce30cc618e0b080
Matt <123038044+MrDudZs@users.noreply.github.com>
2024-04-24 21:42:22 +0100

    Update staffnav.php

--- php/includes/staffnav.php
+++ php/includes/staffnav.php
@@ -1,19 +1,16 @@
 <nav class="top-nav">
     <ul>
         <li><a href="index.php">HOME</a></li>
-        <li><a href="exchange.php">EXCHANGE</a></li>
-        <li><a href="">RATES</a></li>
-        <li><a href="">ABOUT</a></li>
+        <li><a href="php/processes/dashboard.php">DASHBOARD</a></li>
         <li style="float:right"><a href="logout.php">
                 <?php
-                if (isset ($_SESSION['staff_id']) && isset ($_SESSION['email'])) {
-                    // User is logged is
-                    echo "Logged in as: " . $_SESSION['email'];
+
+                if (isset($_SESSION['username'])) {
+                    echo "Logged in: " . $_SESSION['username'];
                 } else {
-                    
-                    header("Location: login.php");
-                    exit;
-                } ?>
+                    echo "Login";
+                }
+                ?>
             </a></li>
     </ul>
 </nav>
