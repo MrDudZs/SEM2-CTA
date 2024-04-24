67a259f3ef3baccee9da15218b0f26b48a357677
Matt <123038044+MrDudZs@users.noreply.github.com>
2024-04-24 21:42:24 +0100

    Update config.php

--- php/config.php
+++ php/config.php
@@ -1,18 +1,15 @@
 <?php
-/* Database credentials. Assuming you are running MySQL
-server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_FIRST_NAME', '');
-define('DB_LAST_NAME', '');
-define('DB_NAME', 'transfer_application');
- 
-/* Attempt to connect to MySQL database */
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_FIRST_NAME, DB_NAME);
- 
-// Check connection
-if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
+
+// Connection
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = 'currency_transfer_app';
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+//Check connection
+
+if ($conn->connect_error) {
+    die ("connection failed: " . $conn->connect_error);
 }
-?>
