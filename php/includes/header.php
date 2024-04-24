2786c206cb5727d1e6f3a38c6645a80c37545c25
Matt <123038044+MrDudZs@users.noreply.github.com>
2024-04-24 21:42:20 +0100

    Update header.php

--- php/includes/header.php
+++ php/includes/header.php
@@ -1,6 +1,6 @@
 
 <header class="header-mn header-mb">
     <div>
-        <img class="desk-logo mob-logo" src="images/CompanyLogoBLK.jpg" alt="Company Logo">
+        <img class="desk-logo mob-logo" src="images/CompanyLogoBLKsml.jpg" alt="Company Logo">
     </div>
 </header>
