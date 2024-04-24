3b413c601123a79e40f928a247774965684745c2
Matt <123038044+MrDudZs@users.noreply.github.com>
2024-04-24 21:41:57 +0100

    Update logout.php

--- logout.php
+++ logout.php
@@ -7,7 +7,7 @@ $_SESSION = [];
 // Destroy the session
 session_destroy();
 
-// Redirect to index.php
-header("Location: index.php");
+// Redirect to login.php
+header("Location: login.php");
 exit;
-?>
+
