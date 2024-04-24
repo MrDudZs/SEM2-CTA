3210ef4183b1163af337553fecbdd1cb96126aa1
Matt <123038044+MrDudZs@users.noreply.github.com>
2024-04-24 21:43:35 +0100

    Update staffloginprocess.php

--- staffloginprocess.php
+++ staffloginprocess.php
@@ -2,63 +2,53 @@
 session_start();
 
 // Connection
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = 'currency_transfer_app';
-
-$conn = new mysqli($servername, $username, $password, $dbname);
+include 'php/config.php';
 
 //Check connection
 
 if ($conn->connect_error) {
-    die ("connection failed: " . $conn->connect_error);
+    die("connection failed: " . $conn->connect_error);
 }
 
-
-
-
-// retreive form data
-$email = $_POST['email'];
+// get form data
 $staffid = $_POST['staff_id'];
 $provided_password = $_POST['password'];
 
-// SQL Query checking staff login
-$sql = "SELECT id FROM staff WHERE email='$email' AND staff_id='$staffid' AND password='$password'";
+// SQL Query checking user login
+$sql = "SELECT staff_id, first_name, surname, password, is_suspended, permission_id FROM staff WHERE staff_id='$staffid'";
 $result = $conn->query($sql);
 
 if ($result->num_rows == 1) {
     // User found, fetch hashed password
     $row = $result->fetch_assoc();
     $hashed_password_from_db = $row['password'];
+    $is_suspended = $row['is_suspended'];
 
     // Verify password
     if (password_verify($provided_password, $hashed_password_from_db)) {
+        if ($is_suspended == 1) {
+            // Account is suspended, redirect back to login page with error message
+            $_SESSION['login_error'] = "Your account is suspended. Please contact support.";
+            header("Location: login.php");
+            exit;
+        } else {
+            // Login successful
+            $_SESSION['staff_id'] = $row['staff_id'];
+            $_SESSION['username'] = $row['first_name'] . ' ' . $row['surname'];
+            $_SESSION['permission_id'] = $row['permission_id'];
 
-
-        // Retrieve user information from login form
-        $email = $_POST['email'];
-        $staffid = $_POST['staff_id'];
-
-        // Combine first and last name
-        // $getname = "SELECT first_name, surname AS whole_name FROM users WHERE email='$email' AND user_id='$userid'";
-
-        // Assuming you have retrieved user information from the database,
-        // set session variables with user data
-        $_SESSION['staff_id'] = $staffid;
-        $_SESSION['email'] = $email;
-
-        // Login successful
-        echo "Login Successful!";
-        header("Location: dashboard.php");
-        exit;
+            header("Location: php/processes/dashboard.php");
+            exit;
+        }
     } else {
-        // if user_id or email is incorrect
-        echo "Invalid email or user ID";
+        // Invalid password
+        $_SESSION['login_error'] = "Invalid email or user ID";
         header("Location: login.php");
         exit;
     }
-
+} else {
+    // User not found
+    $_SESSION['login_error'] = "Invalid email or user ID";
+    header("Location: login.php");
+    exit;
 }
-
-
