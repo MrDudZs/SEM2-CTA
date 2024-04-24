65bc63877042fb37f31035dca508aa384cf8c475
Matt <123038044+MrDudZs@users.noreply.github.com>
2024-04-24 21:41:54 +0100

    Update transferapi.js

--- js/transferapi.js
+++ js/transferapi.js
@@ -1,3 +1,4 @@
+
 function checkInput() { // Deny anything but numbers and decimal points within exchange.php
     var userInput = document.forms["converter"]["Finput"].value;
     var regex = /^[0-9]+$/;
@@ -7,16 +8,37 @@ function checkInput() { // Deny anything but numbers and decimal points within e
     }
 }
 
+// Get input element
+const inputAmount = document.getElementById('input-amount');
+// Get upload section
+const uploadSection = document.getElementById('upload-section');
+// Get transfer input
+const transferBtn = document.getElementById('transfer-btn');
+
+inputAmount.addEventListener('input', function () {
+    const amount = parseFloat(this.value);
+
+    if (amount >= 50.00) {
+        // Show upload section if amount is greater than or equal to 2500.00
+        uploadSection.style.display = amount >= 2500.00 ? 'block' : 'none';
+        // Show transfer btn if ammount is greater than or equeal to 50.00 or less than 1 million
+        transferBtn.style.display = amount >= 50.00 && amount <= 1000000.00 ? 'block' : 'none';
+    } else {
+        // Hide upload section if amount is less than 50.00
+        uploadSection.style.display = 'none';
+        transferBtn.style.display = 'none';
+    }
+});
+
 // Validting fields are not empty
 function validateInput() {
     const inputElement = document.getElementById('input-amount');
-    const inputValue = inputElement.value.trim(); // Remove leading/trailing spaces
+    const inputValue = inputElement.value.trim();
 
     if (inputValue === '') {
         alert('Input field is empty! Please enter something.');
     } else {
         console.log('Input is not empty:', inputValue);
-        // You can perform other actions here if needed
     }
 }
 
@@ -26,14 +48,20 @@ document.getElementById("convert-btn").addEventListener("click", function () {
     const fromCurrency = document.getElementById("from-currency").value;
     const toCurrency = document.getElementById("to-currency").value;
 
-    const apiKey = '';
+    if (fromCurrency === toCurrency) {
+        alert("Please select different wallets for transfer.");
+        return;
+    }
+
+    const apiKey = '3d13fb7c1ae2c7175ca8d47a';
 
     fetch(`https://v6.exchangerate-api.com/v6/${apiKey}/latest/${fromCurrency}`)
         .then(response => response.json())
         .then(data => {
             const exchangeRate = data.conversion_rates[toCurrency];
             const convertedAmount = (amount * exchangeRate).toFixed(2);
-            document.getElementById("result").innerHTML = `${convertedAmount} ${toCurrency}`;
+            var resultInput = document.getElementById("result");
+            resultInput.value = `${convertedAmount}`;
         })
         .catch(error => console.log('Error:', error));
 });
